8dcca10c38a83de9560886753725cda7ea49c513
asthman888 <xiaonan.song@thoughtworks.com>
2026-01-16 10:27:56 +0800

    fix name

--- test_zh.py
+++ test_zh.py
@@ -15,6 +15,6 @@ from chatterbox.mtl_tts import ChatterboxMultilingualTTS
 multilingual_model = ChatterboxMultilingualTTS.from_pretrained(device="cpu")
 
 chinese_text = "没有利润，企业就无法存在。获取利润并不是注定错误的，但利润必须依靠良好的服务而获得，诚实经营的商业企业不可能得不到利润回报。利润不能是基础，它必须是服务的结果。"
-AUDIO_PROMPT_PATH = "NAN.m4a"
+AUDIO_PROMPT_PATH = "SHUN.m4a"
 wav_chinese2 = multilingual_model.generate(chinese_text, language_id="zh", audio_prompt_path=AUDIO_PROMPT_PATH)
-ta.save("zh-nan.wav", wav_chinese2, multilingual_model.sr)
+ta.save("zh-shun.wav", wav_chinese2, multilingual_model.sr)
